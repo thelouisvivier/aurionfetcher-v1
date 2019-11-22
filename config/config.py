0b53ebfa7db543ae8b79029e02dbe744ce2688d7
Louis <22093201+thelouisvivier@users.noreply.github.com>
2019-11-22 19:52:18 +0100

    Create config.py

---
+++ config/config.py
@@ -0,0 +1,15 @@
+#Aurion
+aurion_url = ""
+aurion_username = ""
+aurion_password = ""
+aurion_startdate =
+aurion_endate =
+
+#Telegram
+telegram_token = ""
+telegram_chatid = ""
+
+#Caldav
+caldav_url = ""
+caldav_username = ""
+caldav_password = ""
